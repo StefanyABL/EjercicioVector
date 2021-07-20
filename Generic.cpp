32514010916a0033abc505bceae04dc1c6833a7a
StefanyABL <73035293+StefanyABL@users.noreply.github.com>
2021-07-19 22:47:55 -0500

    Add files via upload

---
+++ Generic.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Generic.cpp
+ * Author: Yuelieth Rodriguez
+ * 
+ * Created on 29 de abril de 2019, 10:29 AM
+ */
+
+#include "Generic.hpp"
+
+Generic::Generic() {
+}
+
+Generic::Generic(const Generic& orig) {
+}
+
+Generic::~Generic() {
+}
+
---
+++ Generic.hpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Generic.hpp
+ * Author: Yuelieth Rodriguez
+ *
+ * Created on 29 de abril de 2019, 10:29 AM
+ */
+
+#ifndef GENERIC_HPP
+#define GENERIC_HPP
+
+class Generic {
+public:
+    Generic();
+    Generic(const Generic& orig);
+    virtual ~Generic();
+private:
+
+};
+
+#endif /* GENERIC_HPP */
+
